273bfc3e9e3f0cc10b09d677c88badabe791af97
giovannifranchi <giovanni.franchi95@gmail.com>
2023-08-08 14:00:21 +0200

    Fix: padding in main

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -16,7 +16,7 @@ $this->beginContent('@backend/views/layouts/base.php')
 
 
     <?php echo $this->render('_aside')?>
-    <main class="flex-grow-1">
+    <main class="flex-grow-1 p-4">
         <?= $content ?>
     </main>
 
