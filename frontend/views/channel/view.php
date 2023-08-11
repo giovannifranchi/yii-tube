6927fe2cc64c425b8420f84f9b5608b61fc0084a
giovannifranchi <giovanni.franchi95@gmail.com>
2023-08-11 14:53:30 +0200

    Add: subscribe button

--- frontend/views/channel/view.php
+++ frontend/views/channel/view.php
@@ -10,9 +10,13 @@ ChannelAsset::register($this);
 ?>
 
 
-<div class="jumbotron d-flex flex-column justify-content-center px-3">
+<div class="jumbotron d-flex flex-column justify-content-center align-items-start px-3 rounded">
     <h1 class="mb-0"><?= $model->username ?></h1>
     <hr class="w-100 m-0" style="border-width: medium;">
+    <button class="btn btn-danger btn-sm mt-3">
+        Subscribe 120
+        <i class="fa-solid fa-bell"></i>
+    </button>
 </div>
 
 
